1095cfffa867de2ac8ff1d72261fd5077c2f6d22
mehmetunlu <munlu4037@gmail.com>
2023-04-22 21:11:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.getElementById('main').remove();
+const newHeader = document.createElement('h1');
+//while composing a new element don t forget to vreate a new
+newHeader.id = 'victory'
+newHeader.innerHTML = "YOUR-NAME is the champion"
+
